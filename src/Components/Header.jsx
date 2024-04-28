808100ef51a7e4f5589207b8f0ba9ba17d8ee98e
Sridhar <sridharmoturu@gmail.com>
2024-04-29 00:12:58 +0530

    header complete

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,3 @@
-import { BsSearch, BsInfoCircle } from "react-icons/bs";
-import { CgShoppingCart } from "react-icons/cg";
-// import { LuMenuSquare } from "react-icons/lu";
-import { TfiHeadphoneAlt } from "react-icons/tfi";
 import { RxCross2 } from "react-icons/rx";
 import { MdKeyboardArrowDown, MdOutlineMenu } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
@@ -11,75 +7,52 @@ const Header = () => {
   const menuItems = [
     {
       id: 1,
-      icon: <CgShoppingCart />,
-      title: "Cart",
+      title: "Platform",
     },
     {
       id: 2,
-      icon: <BsInfoCircle />,
-      title: "About",
+      title: "Company",
     },
     {
       id: 3,
-      icon: <TfiHeadphoneAlt />,
-      title: "Contact",
+      title: "Solutions",
+    },
+    {
+      id: 4,
+      title: "Partners",
+    },
+    {
+      id: 5,
+      title: "Pricing",
     },
   ];
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
   return (
-    /* background styles for header */
-    <header className="bg-glass rounded-3xl mt-5 px-4 md:px-1 mx-8 py-3 sticky top-2">
-      <div className="flex  md:flex-row justify-between md:justify-evenly ">
-        <div className=" flex flex-col md:flex-row gap-3 items-start">
-          <button className="text-xl  font-bold" onClick={() => navigate("/")}>
-            Createnary
-          </button>
-          {/* Input feild */}
-          <div className="relative">
-            <input
-              type="text"
-              name="Creators"
-              placeholder="Search Creators"
-              className="pl-10 pr-4 py-2 w-80 md:w-64 rounded-full justify-self-stretch border border-gray-300 focus:outline-none focus:ring-1 focus:ring-main-800"
-            />
-            <BsSearch className="icon w-5 h-4 absolute top-1/3 left-3 " />
-          </div>
-        </div>
+    <header className="flex justify-between items-center shadow-md p-3">
+      <img src="/logo.png" alt="logo" className="h-16 cursor-pointer select-none"  onClick={() => navigate('/')}/>
+      <div className="effects items-center gap-4 hidden md:flex cursor-pointer">
         {menuItems.map((item) => (
-          <div key={item.id} className="header_element effect">
-            {item.icon}
-            <button>{item.title}</button>
-          </div>
+          <div key={item.id}>{item.title}</div>
         ))}
-        <div className="header_element outline rounded-3xl px-4 py-2 hover:bg-primary hover:text-white ">
-          <button>Login</button>
-          <MdKeyboardArrowDown />
-        </div>
-        <div>
-          <button
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="md:hidden text-3xl"
-          >
-            {!isMenuOpen ? <MdOutlineMenu /> : <RxCross2 />}
-          </button>
-          <div
-            className={`absolute right-0  bg-transparent text-black border border-black rounded-xl px-3 py-2 transition duration-200 ease-in-out md:hidden ${
-              isMenuOpen ? "block" : "hidden"
-            }`}
-          >
-            {menuItems.map((item) => (
-              <div key={item.id} className="flex gap-2 items-center py-1">
-                {item.icon}
-                <button>{item.title}</button>
-              </div>
-            ))}
-            <div className="extraBtn">Signup</div>
-            <div className="extraBtn">Login</div>
-          </div>
-        </div>
+        <button className="outline px-3 py-2 rounded-2xl hover:bg-primary hover:text-white">
+          Admin Login
+        </button>
       </div>
+      <div className="text-3xl md:hidden">
+        <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          {" "}
+          {isMenuOpen ? <MdOutlineMenu /> : <RxCross2 />}
+        </button>
+      </div>
+      {isMenuOpen && (
+        <div className="top-0 right-0 px-10">
+          {menuItems.map((items) => (
+            <div key={items.id}>{items.title}</div>
+          ))}
+        </div>
+      )}
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,26 +4,9 @@ export default {
   theme: {
     extend: {
       colors: {
-        btnColor: "#027ef8",
-        body: "#f8f9fb",
         primary: "#027ef8",
-        primary_text: "#11151d",
-        secondary_text: "#d6d6d6",
-        secondary_text1: "#ccc",
-        secondary_text_alt: "#404040",
-        neutral_grey: "#dddce0",
-        gray: {
-          100: "rgba(17, 21, 29, 0.4)",
-          200: "rgba(17, 21, 29, 0.08)",
-        },
-        steelblue: "#7075b5",
-        gainsboro: "#dee0e0",
-        white: "#fff",
-        glassy: "#dddddd",
-        glass: "rgba(214, 214, 214, 0.4)",
       },
       fontFamily: {
-        urbanist: "Urbanist",
         monoton: ["Monoton"],
         Poppins: "Poppins",
       },
